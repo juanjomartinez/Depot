1245aa64652540142e2147a8cd3cb49081c6843e
Juanjo <juanjo.martinez@sci-spain.com>
2011-01-05 11:33:50 +0000

    Agregados routes y fixtures para login

---
+++ test/functional/sessions_controller_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class SessionsControllerTest < ActionController::TestCase
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+	test "should log in" do
+		dave = users(:one)
+		post :create, :name => dave.name, :password => dave.password
+		assert_redirected_to admin_url
+		assert_equal dave.id, session[:user_id]
+	end
+
+	test "should fail log" do
+		dave = users(:one)
+		post :create, :name => dave.name, :password => 'fail'
+		assert_redirected_to login_url
+	end
+
+	text "should log out" do
+		delete :destroy
+		assert_redirected_to store_url
+	end
+
+end
